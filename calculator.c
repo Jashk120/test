d163847edfed3989c379a13a548dd42a3e2f1190
Ainz <127408603+Jashk120@users.noreply.github.com>
2026-04-28 12:53:14 +0530

    Implement basic arithmetic functions in calculator.c

---
+++ calculator.c
@@ -0,0 +1,12 @@
+int add(int a, int b) {
+    return a + b;
+}
+
+int subtract(int a, int b) {
+    return a - b;
+}
+
+float divide(int a, int b) {
+    if (b == 0) return -1;
+    return (float)a / b;
+}
