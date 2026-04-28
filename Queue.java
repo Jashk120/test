c9bf880b9f3a335b45aafa80273deba428bd3696
Ainz <127408603+Jashk120@users.noreply.github.com>
2026-04-28 12:53:32 +0530

    Implement basic Queue class with enqueue and dequeue

---
+++ Queue.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+public class Queue {
+    private LinkedList<Integer> list = new LinkedList<>();
+
+    public void enqueue(int value) {
+        list.addLast(value);
+    }
+
+    public int dequeue() {
+        return list.removeFirst();
+    }
+
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+}
